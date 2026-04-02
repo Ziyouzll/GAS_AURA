f3aba644d89a6d6a2b3d46ab6a48424b012822c4
Chidng <Chidng2002@outlook.com>
2026-04-02 23:57:06 +0800

    Initial commit (fresh start)

---
+++ Source/GAS/Public/Charactor/AuraCharactor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Charactor/AuraCharacterBase.h"
+#include "AuraCharactor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_API AAuraCharactor : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
